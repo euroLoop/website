5d80f52286634c60a26ae4f7b36fa1dedc601065
p4r4noj4 <p4r4noj4@gmail.com>
2018-03-14 00:35:56 +0000

    Add translations for everything excep team descriptions

--- js/init.js
+++ js/init.js
@@ -1,8 +1,174 @@
-/*
-	Interphase by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
+const translations = {
+  '@metadata': {
+    'authors': [
+      'Igor'
+    ],
+    'last-updated': '2018-03-13',
+    'locale': 'en',
+    'message-documentation': 'qqq'
+  },
+  'en': {
+		// Navigation
+		'euroloop-navbar-about': 'About',
+		'euroloop-navbar-hyperloop': 'Hyperloop',
+    'euroloop-navbar-team': 'Team',
+		'euroloop-navbar-contact': 'Contact',
+		'euroloop-navbar-press': 'Press',
+		'euroloop-navbar-partners': 'Partners',
+
+		// About us
+		'euroloop-about-title': 'About us',
+		'euroloop-about-part1':
+			'EUROLOOP is an e-mobility and infrastructure company based in Poland ' +
+			'currently developing an autonomous cargo transportation and handling ' +
+			'system based on our unique version of Hyperloop technology. ' +
+			'It will use custom technology for the suspension and propulsion systems ' +
+			'to reduce cost of development, construction and operation. ' +
+			'The passenger version is reserved for the future after the cargo system ' +
+			'is deployed and tested.',
+		'euroloop-about-part2':
+			'Previously our founding members participated in the first annual ' +
+			'SpaceX Hyperloop Pod Competition and won one out of 5 main awards - ' +
+			'the Innovation award, as one of 1200 teams from all over the world. ' +
+			'They were part of the only team in the final stage of the competition ' +
+			'not supported by a major university. Since the incorporation people ' +
+			'from other teams in the finals joined us to work on EUROLOOP’s ' +
+			'vision of the Hyperloop technology.',
+		'euroloop-about-part3':
+			'The initial focus for EUROLOOP are the shortest possible routes that ' +
+			'are profitable for cargo transportation and talks are in progress with ' +
+			'numerous partners from maritime and energy sectors interested in ' +
+			'application of our technology for their needs.',
+
+		// Hyperloop
+		'euroloop-hyperloop-title': 'Hyperloop',
+		'euroloop-hyperloop-subtitle': 'The future of transportation is arriving.',
+		'euroloop-hyperloop-description':
+			'The Hyperloop concept takes the best ' +
+			'bits from 3 previous modes of transportation - automotive, air, and rail - ' +
+			'combines them, and removes the bad parts. It will have the safety and speed ' +
+			'of aircraft, capacity of trains, but with the flexibility of cars. ' +
+			'All while remaining low-cost, hassle-free and with a small geographical ' +
+			'footprint. It can also, unlike some previous modes, be constructed to ' +
+			'look beautiful and to be almost completely silent.',
+		'euroloop-hyperloop-city-title': 'City-center to city-center',
+		'euroloop-hyperloop-city-description':
+			'The Hyperloop is silent and has a much smaller geographical footprint ' +
+			'than an airport. This means a terminal can be constructed in the middle ' +
+			'of a city without the noise and space needed for aviation.',
+		'euroloop-hyperloop-logistics-title': 'Faster, cheaper logistics',
+		'euroloop-hyperloop-logistics-description':
+			'The EUROLOOP system will be desiged from the ground up to handle intermodal ' +
+			'container logistics. It can change entirely how container terminals at ' +
+			'seaports are constructed, making them both cheaper and with greater capacity.',
+		'euroloop-hyperloop-world-title': 'Making your world bigger',
+		'euroloop-hyperloop-world-description':
+			'With greatly reduced travel times and city-center to city-center capability, ' +
+			'the Hyperloop can greatly increase the area where people can commute to work from their home.',
+		'euroloop-hyperloop-carbon-title': 'Carbon free',
+		'euroloop-hyperloop-carbon-description':
+			'The Hyperloop is 100% electric. When coupled with renewable energy generation ' +
+			'it can be entirely carbon free. It will also have a smaller impact on the ' +
+			'landscape where it is build since the tube is contructed on pylons rather ' +
+			'than dug into the ground.',
+		'euroloop-hyperloop-fastest-transport': 'Fastest public transport',
+
+		// Team
+		'euroloop-team-title': 'Team',
+
+		// Press
+		'euroloop-press-title': 'Press',
+
+		// Partners
+		'euroloop-partners-title': 'Partners',
+
+		// Contact
+		'euroloop-contact-title': 'Contact Us',
+		'euroloop-contact-address-title': 'Adress',
+		'euroloop-contact-address': 'Jagiellonska 39, Nowy Sacz 33-300 Poland',
+  },
+  'pl': {
+		// Navigation
+		'euroloop-navbar-about': 'O nas',
+		'euroloop-navbar-hyperloop': 'Hyperloop',
+    'euroloop-navbar-team': 'Zespół',
+		'euroloop-navbar-contact': 'Kontakt',
+		'euroloop-navbar-press': 'Prasa',
+		'euroloop-navbar-partners': 'Partnerzy',
+
+		// About us
+		'euroloop-about-title': 'O nas',
+		'euroloop-about-part1':
+			'EUROLOOP jest firmą zajmującą się transportem elektrycznym i infrastrukturą, z siedzibą ' +
+			'w Polsce. Pracujemy nad stworzeniem autonomicznego transportu ' +
+			'towarowego oraz przeładunkowego, bazującego na unikalnej wariacji technologii ' +
+			'Hyperloop. Rozwiązanie to będzie wykorzystywać wyspecjalizowane systemy ' +
+			'napędu oraz podwozia, mające na celu redukcję kosztów rozwoju, konstrukcji ' +
+			'oraz operacji. W przyszłości, gdy system towarowy zostanie wdrożony ' +
+			'i należycie przetestowany, planowana jest wersja pasażerska.',
+		'euroloop-about-part2':
+			'Współzałożyciele EUROLOOP brali udział w pierwszym, corocznym konkursie ' +
+			'SpaceX Hyperloop Pod Competition i zdobyli jedną z pięciu głównych nagród - ' +
+			'nagrodę za Innowację, jako jedna z 1200 drużych z całego świata. ' +
+			'Zespół, którego byli członkami, był jedynym nieuniwersyteckim zespołem w ' +
+			'etapie finałowym konkursu. Członkowie innych finałowych drużyn dołączyli do ' +
+			'EUROLOOP, aby pomóc w pracach nad naszą unikalną wizją technologii Hyperloop.',
+		'euroloop-about-part3':
+			'EUROLOOP z początku skupia się na trasach o krótkich dystansach, ' +
+			'opłacalnych dla transportu towarów. Prowadzimy rozmowy z potencjalnymi ' +
+			'klientami oraz partnerami z sektorów energetycznego oraz transportu morskiego, ' +
+			'którzy wyrazili zainteresowanie wykorzystaniem naszej technologii do swoich potrzeb.',
+
+		// Hyperloop
+		'euroloop-hyperloop-title': 'Hyperloop',
+		'euroloop-hyperloop-subtitle': 'Nadciąga przyszłość transportu.',
+		'euroloop-hyperloop-description':
+			'Koncepcja Hyperloop bierze to co najlepsze z trzech powszechnych środków ' +
+			'transportu - samochodowego, powietrznego oraz kolejowego - łączy je i pozbywa ' +
+			'się tego, co złe. Zawiera bezpieczeństwo oraz szybkość samolotu, pojemność ' +
+			'pociągu oraz elastyczność samochodu. Przy tym wszystkim pozostanie tania, ' +
+			'nieskomplikowana oraz będzie mieć niewielki wpływ na teren i środowisko. ' +
+			'Ponadto, w przeciwieństwie do niektórych z pozostałych środków transportu, ' +
+			'będzie wizualnie zachwycająca oraz niemal bezszelestna.',
+		'euroloop-hyperloop-city-title': 'Z centrum do centrum',
+		'euroloop-hyperloop-city-description':
+			'Hyperloop jest cichy i ma znacznie mniejszy wpływ na teren niż lotnisko, ' +
+			'co oznacza, że terminal może zostać wybudowany w środku miasta, bez ' +
+			'problemów związanych zwyczajowo z samolotami - hałasem i zapotrzebowaniem na teren.',
+		'euroloop-hyperloop-logistics-title': 'Szybsza i tańsza logistyka',
+		'euroloop-hyperloop-logistics-description':
+			'System EUROLOOP jest zbudowany z myślą o intermodalnej logistyce kontenerowej. ' +
+			'Konstrukcja terminali kontenerowych w portach zmieni się na lepsze - będzie ' +
+			'zarazem tańsza, jak i będzie posiadać większą przepustowość.',
+		'euroloop-hyperloop-world-title': 'Twój świat stanie się większy',
+		'euroloop-hyperloop-world-description':
+			'Dzięki istotnej redukcji czasu podróży oraz bezpośrednim połączeniom między centrami ' +
+			'miast, Hyperloop znacznie zwiększy możliwy zasięg transportu pomiędzy domem a pracą.',
+		'euroloop-hyperloop-carbon-title': 'Oparty o odnawialne źródła energii',
+		'euroloop-hyperloop-carbon-description':
+			'Hyperloop jest w 100% elektryczny - w połączeniu z odnawialnymi źródłami energii ' +
+			'będzie on w pełni wolny od węgla. Ponadto będzie miał znacznie mniejszy wpływa ' +
+			'na teren gdzie zostanie zbudowany, gdyż jego tor ustawiony jest na pylonach, ' +
+			'a nie bezpośrednio na ziemii.',
+		'euroloop-hyperloop-fastest-transport': 'Najszybszy transport publiczny',
+
+		// Team
+		'euroloop-team-title': 'Zespół',
+
+		// Press
+		'euroloop-press-title': 'Prasa',
+
+		// Partners
+		'euroloop-partners-title': 'Partnerzy',
+
+		// Contact
+		'euroloop-contact-title': 'Skontaktuj się z nami',
+		'euroloop-contact-address-title': 'Adres',
+		'euroloop-contact-address': 'ul. Jagiellońska 39, Nowy Sącz 33-300, Polska',
+  }
+};
+
+
 (function($) {
 
 	skel.init({
@@ -71,6 +237,18 @@
 	});
 
 	$(window).ready(function(){
+		$.i18n().load(translations).done(function() {
+      // $('.switch-locale').on('click', 'a', function(e) {
+      //   e.preventDefault();
+      //   $.i18n().locale = $(this).data('locale');
+			// 	$('body').i18n();
+      // });
+			let locale = url('?locale');
+			if (locale) {
+				$.i18n().locale = locale;
+			}
+			$('body').i18n();
+    });
 		var banner = $("#banner");
 		if (!skel.vars.isMobile) {
 			banner.addClass("desktop");
